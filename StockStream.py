c5589b8cfe5711d9ea68f1a7bc92fc2c36ade364
vaishnavi3131 <80247836+vaishnavi3131@users.noreply.github.com>
2023-06-10 10:47:42 +0530

    Update StockStream.py

--- StockStream.py
+++ StockStream.py
@@ -44,8 +44,7 @@ if(selected == 'Stocks Performance Comparison'):  # if user selects 'Stocks Perf
         time.sleep(2)
         # st.success('Loaded')
 
-    dict_csv = pd.read_csv('StockStreamTickersData.csv', header=None,
-                           index_col=0).to_dict()  # read csv file
+    dict_csv = pd.read_csv('StockStreamTickersData.csv', header=None, index_col=0).to_dict()[1]  # read csv file
     symb_list = []  # list for storing symbols
     for i in dropdown:  # for each asset selected
         val = dict_csv.get(i)  # get symbol from csv file
